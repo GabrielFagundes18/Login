f47f2e44db65f33c241f18572e8a4ef3022e761b
GabrielFagundes18 <gabrielfadundesvv@gamil.com>
2026-02-26 11:12:20 -0300

    mudando a rota da api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const bcrypt = require("bcrypt")
+import bcrypt from "bcryptjs";
 
 const app = express();
 const PORT = 3001;
